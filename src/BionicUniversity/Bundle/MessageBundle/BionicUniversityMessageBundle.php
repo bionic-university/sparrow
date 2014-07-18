7b35a7c18b6ec22a9b55665d1730ec254e805faf
andrew <anrew.kovalyoff@gmail.com>
2014-07-18 21:48:58 +0300

    Bundle and entities names should be singular

---
+++ src/BionicUniversity/Bundle/MessageBundle/BionicUniversityMessageBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace BionicUniversity\Bundle\MessageBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BionicUniversityMessageBundle extends Bundle
+{
+}
--- src/BionicUniversity/Bundle/MessagesBundle/BionicUniversityMessagesBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace BionicUniversity\Bundle\MessagesBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class BionicUniversityMessagesBundle extends Bundle
-{
-}
