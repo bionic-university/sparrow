86dee11ecc70a8e0ec0174e367959717c60b1c7a
alexander <alexanderkazimirchuk@yahoo.com>
2014-07-22 19:05:52 +0300

    Some problems I solved

--- src/BionicUniversity/Bundle/CommunityBundle/Controller/Front/CommunityController.php
+++ src/BionicUniversity/Bundle/CommunityBundle/Controller/Front/CommunityController.php
@@ -7,13 +7,6 @@ class CommunityController extends Controller
 {
     public function profileAction($id)
     {
-        //$em = $this->getDoctrine()->getManager();
-        //$entity = $em->getRepository('BionicUniversityCommunityBundle:Community')->find($id);
-        //if (!$entity) {
-        //    throw $this->createNotFoundException('Unable to find User entity.');
-        //}
-
-        //return $this->render('BionicUniversityUserBundle:User/Front:profile.html.twig', array('entity'=> $entity));
         return $this->render('BionicUniversityCommunityBundle:Community/Front:profile.html.twig');
     }
 }
