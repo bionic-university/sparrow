7292829b9d106fb27bacf7cae2f85bcd50c0f477
vlad2pac <pepa1346@gmail.com>
2014-07-25 17:50:44 +0300

    Move conifrmation module from admin to front

--- src/BionicUniversity/Bundle/UserBundle/Controller/Admin/UserController.php
+++ src/BionicUniversity/Bundle/UserBundle/Controller/Admin/UserController.php
@@ -2,12 +2,11 @@
 
 namespace BionicUniversity\Bundle\UserBundle\Controller\Admin;
 
-use FOS\UserBundle\Event\FormEvent;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use BionicUniversity\Bundle\UserBundle\Entity\User;
 use BionicUniversity\Bundle\UserBundle\Form\UserType;
-use BionicUniversity\Bundle\UserBundle\Form\CreatePasswordType;
+
 
 /**
  * User controller.
@@ -232,22 +231,5 @@ class UserController extends Controller
     }
 
 
-    public function createPasswordAction(Request $request)
-    {
-        $entity = $this->getUser();
-        $form = $this->createForm(new CreatePasswordType(), $entity, array(
-            'action' => $this->generateUrl('fos_user_registration_confirmed'),
-            'method' => 'POST'
-        ));
-        $form->handleRequest($request);
-        if ($form->isValid()) {
-            $userManager = $this->get("fos_user.user_manager");
-            $userManager->updateUser($this->getUser());
-            return $this->redirect($this->generateUrl('user_profile',array(
-                'id' => $this->getUser()->getId())));
-        }
-        return $this->render("BionicUniversityUserBundle:User/Admin:create_password.html.twig", array(
-            'form' => $form->createView()
-        ));
-    }
+
 }
--- src/BionicUniversity/Bundle/UserBundle/Controller/Front/UserController.php
+++ src/BionicUniversity/Bundle/UserBundle/Controller/Front/UserController.php
@@ -1,12 +1,33 @@
 <?php
 namespace BionicUniversity\Bundle\UserBundle\Controller\Front;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use BionicUniversity\Bundle\UserBundle\Form\CreatePasswordType;
 
 class UserController extends Controller
 {
     public function profileAction($id)
     {
         return $this->render('BionicUniversityUserBundle:User/Front:profile.html.twig');
+    }
+
+    public function createPasswordAction(Request $request)
+    {
+        $entity = $this->getUser();
+        $form = $this->createForm(new CreatePasswordType(), $entity, array(
+            'action' => $this->generateUrl('fos_user_registration_confirmed'),
+            'method' => 'POST'
+        ));
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            $userManager = $this->get("fos_user.user_manager");
+            $userManager->updateUser($this->getUser());
+            return $this->redirect($this->generateUrl('user_profile',array(
+                'id' => $this->getUser()->getId())));
+        }
+        return $this->render("BionicUniversityUserBundle:User/Front:create_password.html.twig", array(
+            'form' => $form->createView()
+        ));
     }
 }
