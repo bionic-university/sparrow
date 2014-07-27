928dfe6d6c489fee5ba966a6d02efe64087d9fb4
alexander <alexanderkazimirchuk@yahoo.com>
2014-07-27 18:37:38 +0300

    Communities list, communities show, communities joining, membership admin panel

--- src/BionicUniversity/Bundle/CommunityBundle/Controller/Front/CommunityController.php
+++ src/BionicUniversity/Bundle/CommunityBundle/Controller/Front/CommunityController.php
@@ -1,6 +1,7 @@
 <?php
 namespace BionicUniversity\Bundle\CommunityBundle\Controller\Front;
 
+use BionicUniversity\Bundle\CommunityBundle\Entity\Membership;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class CommunityController extends Controller
@@ -23,7 +24,47 @@ class CommunityController extends Controller
 
         $em = $this->getDoctrine()->getManager();
         $myMemberships = $em->getRepository('BionicUniversityCommunityBundle:Membership')->findByUser($user);
+        $all = $em->getRepository('BionicUniversityCommunityBundle:Community')->findAll();
+        $allCommunities=array();
+        foreach ($all as $community)
+        {
+            foreach($myMemberships as $membership)
+            {
+                if($community->getId() != $membership->getCommunity()->getId())
+                {
+                    array_push($allCommunities,$community);
+                }
+            }
+        }
 
-        return $this->render('BionicUniversityCommunityBundle:Community/Front:communities.html.twig', array('my_memberships'=>$myMemberships));
+        return $this->render('BionicUniversityCommunityBundle:Community/Front:communities.html.twig', array('my_memberships'=>$myMemberships,'all_communities'=>$allCommunities));
+    }
+
+    public function joinAction($id)
+    {
+        $entity = new Membership();
+        $entity->setUser($this->getUser());
+        $em = $this->getDoctrine()->getManager();
+
+        $community = $em->getRepository('BionicUniversityCommunityBundle:Community')->findOneById($id);
+        $entity->setCommunity($community);
+
+        $em->persist($entity);
+        $em->flush();
+
+        return $this->redirect($this->generateUrl('communities'));
+    }
+
+    public function leaveAction($id)
+    {
+
+        $em = $this->getDoctrine()->getManager();
+
+        $membership = $em->getRepository('BionicUniversityCommunityBundle:Membership')->findOneById($id);
+
+        $em->remove($membership);
+        $em->flush();
+
+        return $this->redirect($this->generateUrl('communities'));
     }
 }
