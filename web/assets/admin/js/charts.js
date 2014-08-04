95911173306c3f0850252736b3909f8a53a6687f
Andrew Kovalyov <andrew.kovalyoff@gmail.com>
2014-08-04 15:47:08 +0300

    JS fix

--- web/assets/admin/js/charts.js
+++ web/assets/admin/js/charts.js
@@ -1,40 +1,41 @@
 (function ($) {
     $(function () {
-        $('#articles').highcharts({
-            chart: {
-                type: 'spline'
-            },
-            title: {
-                text: 'Articles creation frequency'
-            },
-            subtitle: {
-                text: 'Sparrow.com'
-            },
-            xAxis: {
-                type: 'datetime',
-                dateTimeLabelFormats: { // don't display the dummy year
-                    month: '%e. %b',
-                    year: '%b'
+        if (typeof articlesData != 'undefined') {
+            $('#articles').highcharts({
+                chart: {
+                    type: 'spline'
                 },
                 title: {
-                    text: 'Date'
-                }
-            },
-            yAxis: {
-                title: {
-                    text: 'Quantity'
+                    text: 'Articles creation frequency'
                 },
-                min: 0
-            },
-            tooltip: {
-                headerFormat: '<b>{series.name}</b><br>',
-                pointFormat: '{point.x:%e. %b}: {point.y:.2f} m'
-            },
+                subtitle: {
+                    text: 'Sparrow.com'
+                },
+                xAxis: {
+                    type: 'datetime',
+                    dateTimeLabelFormats: { // don't display the dummy year
+                        month: '%e. %b',
+                        year: '%b'
+                    },
+                    title: {
+                        text: 'Date'
+                    }
+                },
+                yAxis: {
+                    title: {
+                        text: 'Quantity'
+                    },
+                    min: 0
+                },
+                tooltip: {
+                    headerFormat: '<b>{series.name}</b><br>',
+                    pointFormat: '{point.x:%e. %b}: {point.y:.2f} m'
+                },
 
-            series: [
-                articlesData
-            ]
-        });
+                series: [
+                    articlesData
+                ]
+            });
+        }
     });
-
 })(jQuery);
