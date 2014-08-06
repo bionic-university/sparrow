5d69acee9a2d0d5dec47a2d66a597a08abada105
repzy <deneir@i.ua>
2014-08-06 18:23:18 +0300

    Posts fetching fix

--- src/BionicUniversity/Bundle/UserBundle/Controller/Front/UserController.php
+++ src/BionicUniversity/Bundle/UserBundle/Controller/Front/UserController.php
@@ -21,7 +21,7 @@ class UserController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
         $entity = $em->getRepository('BionicUniversityUserBundle:User')->find($id);
-        $posts = $em->getRepository('BionicUniversityWallBundle:Post')->findByAuthor($entity);
+        $posts = $em->getRepository('BionicUniversityWallBundle:Post')->findBy(['author'=>$entity, 'community'=>null], ['createdAt'=>'desc' ]);
 
         if (!$entity) {
             throw $this->createNotFoundException('Unable to find User entity.');
@@ -29,6 +29,7 @@ class UserController extends Controller
 
         $form = $this->createPostForm();
         $csrfToken = $this->get('form.csrf_provider')->generateCsrfToken('delete_post');
+
 
         return $this->render('BionicUniversityUserBundle:User/Front:profile.html.twig', array(
             'entity' => $entity,
